94eca2c4c6cfa0c4424e0c50c9b5b9111ae22b8b
Dominik Bolla <dominikbolla@gmail.com>
2022-11-19 00:50:26 +0100

    LINQ Ramen Feladatmegoldás

---
+++ LINQ_Ramen/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace LINQ_Ramen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filterBox = new System.Windows.Forms.TextBox();
+            this.countryList = new System.Windows.Forms.ListBox();
+            this.RamenDGW = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.RamenDGW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // filterBox
+            // 
+            this.filterBox.Location = new System.Drawing.Point(13, 13);
+            this.filterBox.Name = "filterBox";
+            this.filterBox.Size = new System.Drawing.Size(192, 20);
+            this.filterBox.TabIndex = 0;
+            this.filterBox.TextChanged += new System.EventHandler(this.filterBox_TextChanged);
+            // 
+            // countryList
+            // 
+            this.countryList.FormattingEnabled = true;
+            this.countryList.Location = new System.Drawing.Point(13, 44);
+            this.countryList.Name = "countryList";
+            this.countryList.Size = new System.Drawing.Size(192, 394);
+            this.countryList.TabIndex = 1;
+            this.countryList.SelectedIndexChanged += new System.EventHandler(this.countryList_SelectedIndexChanged);
+            // 
+            // RamenDGW
+            // 
+            this.RamenDGW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RamenDGW.Location = new System.Drawing.Point(212, 44);
+            this.RamenDGW.Name = "RamenDGW";
+            this.RamenDGW.Size = new System.Drawing.Size(576, 394);
+            this.RamenDGW.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RamenDGW);
+            this.Controls.Add(this.countryList);
+            this.Controls.Add(this.filterBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.RamenDGW)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filterBox;
+        private System.Windows.Forms.ListBox countryList;
+        private System.Windows.Forms.DataGridView RamenDGW;
+    }
+}
+
---
+++ LINQ_Ramen/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ_Ramen
+{
+    public partial class Form1 : Form
+    {
+        private List<Country> countries = new List<Country>();
+        private List<Ramen> ramens = new List<Ramen>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData("ramen.csv");
+            countryList.DisplayMember = "Name";
+            GetCountries();
+        }
+
+        private void LoadData(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName, Encoding.Default);
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                var sor = sr.ReadLine().Split(';');
+
+                var sorCountry = sor[2];
+                var currCountry = AddCountry(sorCountry);
+                var ramen = new Ramen()
+                {
+                    Brand = sor[0],
+                    Name = sor[1],
+                    ID = ramens.Count + 1,
+                    CountryFK = currCountry.ID,
+                    Country = currCountry,
+                    Stars = Convert.ToDouble(sor[3])
+                };
+                ramens.Add(ramen);
+
+            }
+        }
+
+        private Country AddCountry(string sorCountry)
+        {
+            var currentCountry = (from x in countries
+                                  where x.Name.Equals(sorCountry)
+                                  select x).FirstOrDefault();
+
+            if (currentCountry == null)
+            {
+                currentCountry = new Country()
+                {
+                    ID = countries.Count + 1,
+                    Name = sorCountry
+                };
+                countries.Add(currentCountry);
+            }
+            return currentCountry;
+        }
+
+        private void GetCountries()
+        {
+            var countriesFiltered = from x in countries
+                                where x.Name.Contains(filterBox.Text)
+                                orderby x.Name
+                                select x;
+            countryList.DataSource = countriesFiltered.ToList();
+        }
+
+        private void filterBox_TextChanged(object sender, EventArgs e)
+        {
+            GetCountries();
+        }
+
+        private void countryList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var countrySelected = (Country)((ListBox)sender).SelectedItem;
+
+            if (countrySelected == null)
+                return;
+
+            var RamenByCountry = from r in ramens
+                                where r.CountryFK == countrySelected.ID
+                                select r;
+
+            var RamenGrouped = from r in RamenByCountry
+                                group r.Stars by r.Brand into g
+                                select new
+                                {
+                                    Márkanév = g.Key,
+                                    Értékátlag = Math.Round(g.Average(), 2)
+                                };
+
+            var RatingOrderedGroups = from g in RamenGrouped
+                                orderby g.Értékátlag descending
+                                select g;
+
+            RamenDGW.DataSource = RatingOrderedGroups.ToList();
+        }
+    }
+}
